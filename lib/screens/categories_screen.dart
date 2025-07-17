a756ad2c28e223c5504bff470fb6301e30c1a6e2
ranaahmed786 <ranamuhammadahmed6@gmail.com>
2025-07-17 20:13:00 +0500

    Initial project setup and data models , screens and widgets initialised , categories screen developed

---
+++ lib/screens/categories_screen.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_meals_app/widgets/category_card.dart';
+import 'package:flutter_meals_app/data/categories_data.dart';
+
+class CategoriesScreen extends StatelessWidget {
+  const CategoriesScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Categories')),
+      body: GridView(
+        padding: const EdgeInsets.all(20),
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          childAspectRatio: 3 / 2,
+          crossAxisSpacing: 20,
+          mainAxisSpacing: 20,
+        ),
+        children: [
+          for (final category in availableCategories)
+            CategoryCard(category: category),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/widgets/category_card.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_meals_app/main.dart';
+import 'package:flutter_meals_app/models/category.dart';
+
+class CategoryCard extends StatelessWidget {
+  final Category category;
+  const CategoryCard({super.key, required this.category});
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      splashColor: theme.colorScheme.primary.withAlpha(160),
+      onTap: () {},
+      borderRadius: BorderRadius.circular(10),
+      child: Container(
+        padding: const EdgeInsets.all(16),
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [
+              category.color.withAlpha(120),
+              category.color.withAlpha(255),
+            ],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+          ),
+          borderRadius: BorderRadius.circular(10),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black.withAlpha(75),
+              spreadRadius: 2,
+              blurRadius: 8,
+              offset: const Offset(0, 4),
+            ),
+          ],
+        ),
+        child: Text(
+          category.title,
+          style: Theme.of(context).textTheme.titleLarge!.copyWith(
+            color: Colors.white,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+    );
+  }
+}
